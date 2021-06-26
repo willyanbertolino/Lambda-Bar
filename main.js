b88c798de3c6d2bbb047c1919c0aaa4d09c8ae24
Willyan Bertolino <willyanhpbertolino@gmail.com>
2021-06-26 09:03:38 -0300

    navbar

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// setup nav
+const navBtn = document.getElementById('nav-btn');
+const navbar = document.getElementById('navbar');
+const navClose = document.getElementById('nav-close');
+
+// show nav
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('show-nav');
+});
+
+// close nav
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('show-nav');
+});
+
 // setup date
 const date = (document.getElementById(
   'date'
